a5780f357293e791245d416587e5e67cc586891c
MohammadMahdi Arbabpouri <sardarcyberym@gmail.com>
2023-06-19 10:29:17 +0330

    change strings

--- strings.py
+++ strings.py
@@ -2,14 +2,13 @@ from typing import Iterable
 
 class Strings:
     
-    START = "a"
-    GET_MESSAGE = "b"
-    GET_URL = "c"
-    BAD_URL = "d"
-    BAD_MESSAGE = "e"
-    CONNECT = "f"
-    CANCELED = "g"
-    CHANNEL_LIST_IS_EMPTY = "ss"
+    START: str = "❣️ سلام ادمین عزیز به ربات خودت خوش آمدی سَرورم"
+    GET_MESSAGE: str = "♨️ پیامی که میخواهید در کانال نمایش دهید ارسال کنید.\n\n/cancel"
+    GET_URL: str = "🌐 لینک نمایش را به صورت صحیح ارسال کنید برای دکمه\n\n/cancel"
+    BAD_URL: str = "🚫 لینکی ارسالی شما دارای مشکل است و از طرف تلگرام رد میشود.\n\n/cancel"
+    CONNECT: str = "💢 اتصال به پروکسی 💢"
+    CANCELED: str = "⭕️ عملیات کنسل شد ⭕️"
+    CHANNEL_LIST_IS_EMPTY: str = "⚠️ کانالی برای ارسال وجود ندارد "
 
     @staticmethod
     def channels_send_status(sended: Iterable[list | set], not_sended: Iterable[list | set]) -> str:
@@ -19,9 +18,8 @@ class Strings:
         not_sended = map(func, not_sended)
 
         text = (
-            f"s : {'-'.join(sended)}"
-            "\n\n"
-            f"n : {'-'.join(not_sended)}"
+            f"💢 برای کانال های {'-'.join(sended)} ارسال شد\n\n"
+            f"‼️ برای کانال های {'-'.join(not_sended)} ارسال نشد"
         )
         return text
     
